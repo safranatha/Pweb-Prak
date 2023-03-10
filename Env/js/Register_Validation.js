382508375cbd625aa65bfc7fa0e04ae247da71de
AlfarizaRasendria <rasendria.alfariza18@gmail.com>
2023-03-10 17:34:01 +0700

    Update Regist Page Validation JS

--- Env/js/Register_Validation.js
+++ Env/js/Register_Validation.js
@@ -14,9 +14,9 @@
 })()
 
 
-function HidePlaceHolder (Element){
-  Element.addEventListener("click",function(){
-      Element.placeholder="";
+function HidePlaceHolder(Element) {
+  Element.addEventListener("click", function () {
+    Element.placeholder = "";
   })
 }
 
@@ -25,10 +25,7 @@ var Fullname_Register = document.getElementById("Fullname_Register")
 var Email_Register = document.getElementById("Email_Register")
 var PhoneNumber_Register = document.getElementById("PhoneNumber_Register")
 var password_Register = document.getElementById("password_Register")
-const Icon1 = document.querySelector("#Full_Name");
-const Icon2 = document.querySelector("#Email_2");
-const Icon3 = document.querySelector("#Phone");
-const Icon4 = document.querySelector("#Password");
+
 
 
 HidePlaceHolder(Fullname_Register)
@@ -37,74 +34,93 @@ HidePlaceHolder(PhoneNumber_Register)
 HidePlaceHolder(password_Register)
 
 
+const Icon1 = document.querySelector("#Full_Name");
+const Icon2 = document.querySelector("#Email_2");
+const Icon3 = document.querySelector("#Phone");
+const Icon4 = document.querySelector("#Password");
 
-form.addEventListener('submit', function (e) {
-e.preventDefault();
-checkInputs();
+form.addEventListener('submit', function () {
+  checkInputs();
 });
 
 function checkInputs() {
-const Fullname_Register_Value = Fullname_Register.value.trim();
-const Email_Register_Value = Email_Register.value.trim();
-const Phone_Register_Value = PhoneNumber_Register.value.trim();
-const password_Register_Value = password_Register.value.trim();
+  const Fullname_Register_Value = Fullname_Register.value.trim();
+  const Email_Register_Value = Email_Register.value.trim();
+  const Phone_Register_Value = PhoneNumber_Register.value.trim();
+  const password_Register_Value = password_Register.value.trim();
 
 
-if (Fullname_Register_Value === '') {
-  setErrorFor(Fullname_Register, "Username cannot be blank");
-}
-else {
-  setSuccessFor(Fullname_Register);
-}
+  let hasError = false;
+  if (Fullname_Register_Value === '') {
+    setErrorFor(Fullname_Register, "Username cannot be blank");
+    hasError = true;
 
-if (Email_Register_Value === '') {
-  setErrorFor(Email_Register, "Email cannot be blank");
-} 
-if (Email_Register_Value === '') {
-  setErrorFor(Email_Register, "Email cannot be blank");
-}
-else if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(Email_Register_Value))){
-  setErrorFor(Email_Register, "Please enter a valid email address");
-}
-else {
-  setSuccessFor(Email_Register);
-}
-
-
-if (Phone_Register_Value === '') {
-  setErrorFor(Phone_Register, "Phone cannot be blank");
-}
-else if (!(/^[0-9]{10,13}$/.test(Phone_Register_Value))) {
-  setErrorFor(PhoneNumber_Register, "Please enter a valid phone number");
-} 
+  }
   else {
-  setSuccessFor(Phone_Register);
+    setSuccessFor(Fullname_Register);
+  }
+
+  if (Email_Register_Value === '') {
+    setErrorFor(Email_Register, "Email cannot be blank");
+    hasError = true;
+  }
+  else if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(Email_Register_Value))) {
+    setErrorFor(Email_Register, "Please enter a valid email address");
+    hasError = true;
+  }
+  else {
+    setSuccessFor(Email_Register);
+  }
+
+
+  if (Phone_Register_Value === '') {
+    setErrorFor(Phone_Register, "Phone cannot be blank");
+    hasError = true;
+  }
+  else if (!(/^[0-9]{10,13}$/.test(Phone_Register_Value))) {
+    setErrorFor(PhoneNumber_Register, "Please enter a valid phone number");
+    hasError = true;
+  }
+  else {
+    setSuccessFor(Phone_Register);
+  }
+
+  if (password_Register_Value === '') {
+    setErrorFor(password_Register, "Password cannot be blank");
+    hasError = true;
+  } else if (password_Register_Value.length < 8) {
+    setErrorFor(password_Register, "Password must be at least 8 characters long");
+    hasError = true;
+  } else {
+    setSuccessFor(password_Register);
+  }
+
+  if (!hasError) {
+    form.submit();
+  }
+  else {
+    form.preventDefault();
+  }
 }
 
-if (password_Register_Value === '') {
-  setErrorFor(password_Register, "Password cannot be blank");
-} else if (password_Register_Value.length < 8) {
-  setErrorFor(password_Register, "Password must be at least 8 characters long");
-} else {
-  setSuccessFor(password_Register);
-  form.submit();
-}
-}
 
 function setErrorFor(input, message) {
-const formControl = input.parentElement;
-const feedback = formControl.querySelector("p");
-feedback.textContent = message;
-feedback.style.visibility = "visible"
-Icon1.style.paddingBottom = "35px";
-Icon2.style.paddingBottom = "35px";
-Icon3.style.paddingBottom = "35px";
-Icon4.style.paddingBottom = "35px";
-
+  const formControl = input.parentElement;
+  const feedback = formControl.querySelector("p");
+  feedback.textContent = message;
+  feedback.style.visibility = "visible"
+  const IconList = [Icon1, Icon2, Icon3, Icon4];
+  for (let i = 0; i < IconList.length; i++) {
+    IconList[i].style.paddingBottom = "35px";
+  }
 }
 
 function setSuccessFor(input) {
-const formControl = input.parentElement;
-const feedback = formControl.querySelector("p");
-feedback.style.visibility = "hidden"
+  const formControl = input.parentElement;
+  const feedback = formControl.querySelector("p");
+  feedback.style.visibility = "hidden"
+  const IconList = [Icon1, Icon2, Icon3, Icon4];
+  for (let i = 0; i < IconList.length; i++) {
+    IconList[i].style.paddingBottom = "35px";
+  }
 }
